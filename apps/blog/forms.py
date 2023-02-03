bf533524923b270c22b89db1b73b9fabcd2edf3a
demirug <pm.antondolia@gmail.com>
2023-02-03 15:58:07 +0200

    Add Blog create form

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -2,9 +2,23 @@ from flask_wtf import FlaskForm
 import wtforms
 import wtforms.validators as validators
 
+from apps.blog import Blog
+from services.unique_slugify import unique_slugify
+
 
 class BlogForm(FlaskForm):
-    username = wtforms.StringField("Username",
-                                   validators=[validators.Length(5, 32), validators.InputRequired()])
-    password = wtforms.PasswordField("Password", validators=[validators.Length(9, 24), validators.InputRequired()])
-    remember_me = wtforms.BooleanField("Remember me", default=False)
+    slug = wtforms.StringField("Slug", validators=[
+        validators.Regexp('^[a-z0-9-]*$',
+                          message="Slug allowed only lowercase letters, numbers, -")
+    ])
+    title = wtforms.StringField("Title", validators=[validators.InputRequired()])
+    content = wtforms.TextAreaField("Content", validators=[validators.InputRequired()])
+
+    def validate_slug(self, field):
+
+        if field.data == "":
+            field.data = unique_slugify(Blog(), self.title.data)
+        elif Blog.query.filter_by(slug=field.data).first():
+            raise validators.ValidationError("Given slug already in use")
+
+        return field
